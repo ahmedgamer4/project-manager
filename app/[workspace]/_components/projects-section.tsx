d281ea909f144805ccdf859a3eb3cb22082bf5c5
ahmergamer4 <ahmedalseidy4@gmail.com>
2025-08-05 15:33:49 +0300

    feat(projects): add project deleting functionality

--- app/[workspace]/_components/projects-section.tsx
+++ app/[workspace]/_components/projects-section.tsx
@@ -1,13 +1,23 @@
 'use client';
 
 import { zodResolver } from '@hookform/resolvers/zod';
-import { ChevronDown, Loader2, Plus } from 'lucide-react';
+import { ChevronDown, Loader2, Plus, Trash } from 'lucide-react';
 import { useParams } from 'next/navigation';
 import { useMemo, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import type { z } from 'zod';
 import { trpc } from '@/app/_trpc/client';
 import { Loading } from '@/components/loading';
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
 import { Button } from '@/components/ui/button';
 import {
   Dialog,
@@ -52,7 +62,13 @@ export function ProjectsSection() {
 
   const trpcUtils = trpc.useUtils();
   const [isExpanded, setIsExpanded] = useState(true);
-  const { data: projects, isLoading, isError } = trpc.projects.all.useQuery();
+  const {
+    data: projects,
+    isLoading,
+    isError,
+  } = trpc.projects.all.useQuery({
+    workspaceId: workspace as string,
+  });
   const createProject = trpc.projects.create.useMutation({
     onSuccess: () => {
       trpcUtils.projects.all.invalidate();
@@ -93,35 +109,53 @@ export function ProjectsSection() {
             </button>
           </SidebarGroupLabel>
           <DialogTrigger asChild>
-            <Button
-              className="h-6 w-6"
-              onClick={(e) => {
-                e.stopPropagation();
-                // Add new project logic
-              }}
-              size="icon"
-              variant="ghost"
-            >
+            <Button className="h-6 w-6" size="icon" variant="ghost">
               <Plus className="h-3 w-3" />
             </Button>
           </DialogTrigger>
         </div>
 
         {isExpanded && (
-          <SidebarGroupContent className="px-2">
-            <SidebarMenu>
-              {projects?.map((p) => (
-                <SidebarMenuItem key={p.id}>
-                  <SidebarMenuButton>
-                    <div className="flex h-4 w-4 items-center justify-center rounded-sm bg-primary p-2 text-primary-foreground text-sm">
-                      {p.name.charAt(0)}
-                    </div>
-                    <span>{p.name}</span>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
+          <AlertDialog key={'project-delete-dialog'}>
+            <SidebarGroupContent className="px-2">
+              <SidebarMenu>
+                {projects?.map((p) => (
+                  <SidebarMenuItem key={p.id}>
+                    <SidebarMenuButton className="justify-between">
+                      <div className="flex items-center gap-2">
+                        <div className="flex h-4 w-4 items-center justify-center rounded-sm bg-primary p-2 text-primary-foreground text-sm">
+                          {p.name.charAt(0)}
+                        </div>
+                        <span>{p.name}</span>
+                      </div>
+                      <AlertDialogTrigger asChild>
+                        <Button
+                          className="h-6 w-6 hover:bg-destructive/10 hover:text-destructive"
+                          size="icon"
+                          variant="ghost"
+                        >
+                          <Trash className="h-3 w-3" />
+                        </Button>
+                      </AlertDialogTrigger>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>Delete Project</AlertDialogTitle>
+              </AlertDialogHeader>
+              <AlertDialogDescription>
+                Are you sure you want to delete this project? This action cannot
+                be undone.
+              </AlertDialogDescription>
+              <AlertDialogFooter>
+                <Button variant="destructive">Delete</Button>
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
         )}
       </SidebarGroup>
       <DialogContent>
